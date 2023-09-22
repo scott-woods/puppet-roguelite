54ca8f9e0bb76952b7a84d3b08dd2fb18dde7039
scott-woods <scott_woods44@yahoo.com>
2023-09-21 20:19:09 -0400

    Added nopipeline and setup Game1

---
+++ PuppetRoguelite/Scenes/TestScene.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Nez;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuppetRoguelite.Scenes
+{
+    public class TestScene : Scene
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            ClearColor = Color.Black;
+        }
+    }
+}
