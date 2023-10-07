f4d5bb67e5755d6d699d2ff4b85817e1a68c1e0c
scott-woods <scott_woods44@yahoo.com>
2023-10-06 22:43:25 -0400

    working on ui remodel

--- PuppetRoguelite/UI/HUDs/CombatUI.cs
+++ PuppetRoguelite/UI/HUDs/CombatUI.cs
@@ -20,6 +20,9 @@ namespace PuppetRoguelite.UI.HUDs
         Label _playerHealthLabel;
         ProgressBar _apProgressBar;
         Label _playerApLabel;
+        Table _topLeftTable;
+        Table _topRightTable;
+        Table _bottomMiddleTable;
 
         //skins
         Skin _basicSkin;
@@ -31,7 +34,7 @@ namespace PuppetRoguelite.UI.HUDs
             //base table
             _table = Stage.AddElement(new Table());
             _table.SetFillParent(true);
-            _table.SetDebug(false);
+            _table.SetDebug(true);
 
             //load skin
             _basicSkin = Skin.CreateDefaultSkin();
@@ -52,26 +55,51 @@ namespace PuppetRoguelite.UI.HUDs
         void ArrangeElements()
         {
             //status section in top left
-            var statusTable = new Table();
-            statusTable.Defaults().Left().SetSpaceBottom(5);
-            _table.Add(statusTable).Expand().Top().Left().SetPadTop(5).SetPadLeft(5);
+            //var statusTable = new Table();
+            //statusTable.Defaults().Left().SetSpaceBottom(5);
+            //_table.Add(statusTable).Expand().Top().Left().SetPadTop(5).SetPadLeft(5);
+            //_playerHealthLabel = new Label("HP: ", _basicSkin);
+            //statusTable.Add(_playerHealthLabel);
+            //statusTable.Row();
+            //statusTable.Add(new Label("$", _basicSkin));
+            //statusTable.Row();
+            //statusTable.Add(new Label("&", _basicSkin));
+
+            _topLeftTable = new Table();
+            _table.Add(_topLeftTable).Expand().Top().Left().SetPadTop(5).SetPadLeft(5);
             _playerHealthLabel = new Label("HP: ", _basicSkin);
-            statusTable.Add(_playerHealthLabel);
-            statusTable.Row();
-            statusTable.Add(new Label("$", _basicSkin));
-            statusTable.Row();
-            statusTable.Add(new Label("&", _basicSkin));
+            _topLeftTable.Add(_playerHealthLabel);
+            _topLeftTable.Pack();
+
+            _topRightTable = new Table();
+            _table.Add(_topRightTable).Expand().Top().Right().SetPadTop(5).SetPadRight(5);
+            _topRightTable.Add(new Label("$", _basicSkin));
+            _topRightTable.Pack();
+
+            _table.Row();
+
+            _bottomMiddleTable = new Table();
+            _table.Add(_bottomMiddleTable).Expand().Bottom().SetColspan(2).Width(480 * .35f);
+            var apGroup = new Table();
+            apGroup.Defaults().SetSpaceRight(8).Grow();
+            //apGroup.SetWidth(5);
+            _bottomMiddleTable.Add(apGroup).Grow().SetPadBottom(5);
+            apGroup.Add(new TextButton("hi", _basicSkin)).Expand();
+            apGroup.Add(new TextButton("hi", _basicSkin)).Expand();
+            apGroup.Add(new TextButton("hi", _basicSkin)).Expand();
+            //apGroup.Pack();
+            //_bottomMiddleTable.Pack();
 
             //ap table
-            _table.Row();
-            var apTable = new Table();
-            apTable.Defaults().Center();
-            _table.Add(apTable).Expand().Bottom().SetPadBottom(10);
-            _playerApLabel = new ApLabel("/", _basicSkin);
-            apTable.Add(_playerApLabel).SetSpaceBottom(5);
-            apTable.Row();
-            _apProgressBar = new ApProgressBar(_basicSkin);
-            apTable.Add(_apProgressBar);
+            //_table.Row();
+            //var apTable = new Table();
+            //apTable.Defaults().Center();
+            //_table.Add(apTable).Expand().Bottom().SetPadBottom(10);
+            //_playerApLabel = new ApLabel("/", _basicSkin);
+            //apTable.Add(_playerApLabel).SetSpaceBottom(5);
+            //apTable.Row();
+            //_apProgressBar = new ApProgressBar(_basicSkin);
+            //apTable.Add(_apProgressBar);
         }
 
         void ConnectToEmitters()
@@ -103,6 +131,7 @@ namespace PuppetRoguelite.UI.HUDs
         public void OnPlayerHealthChanged(HealthComponent healthComponent)
         {
             _playerHealthLabel.SetText($"HP: {healthComponent.Health}/{healthComponent.MaxHealth}");
+            _topLeftTable.Pack();
         }
     }
 }
