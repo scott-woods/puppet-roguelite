2d9ac73215d94e1b9b224a8e9f9f3967d97d4352
scott-woods <scott_woods44@yahoo.com>
2023-11-11 00:07:39 -0500

    started working on automapping

--- PuppetRoguelite/Content/Tiled/Tilesets/dungeon_prison_tileset.tsx
+++ PuppetRoguelite/Content/Tiled/Tilesets/dungeon_prison_tileset.tsx
@@ -7,21 +7,23 @@
   </objectgroup>
  </tile>
  <wangsets>
-  <wangset name="terrains" type="corner" tile="-1">
-   <wangcolor name="stone" color="#ff0000" tile="-1" probability="1"/>
-   <wangtile tileid="4" wangid="0,0,0,1,0,0,0,0"/>
-   <wangtile tileid="5" wangid="0,0,0,1,0,1,0,0"/>
-   <wangtile tileid="6" wangid="0,0,0,0,0,1,0,0"/>
-   <wangtile tileid="7" wangid="0,1,0,0,0,1,0,1"/>
-   <wangtile tileid="9" wangid="0,1,0,1,0,0,0,1"/>
-   <wangtile tileid="29" wangid="0,1,0,1,0,0,0,0"/>
+  <wangset name="Dungeon Ground" type="corner" tile="30">
+   <wangcolor name="Stone" color="#ff0000" tile="30" probability="1"/>
+   <wangcolor name="StoneShadow" color="#00ff00" tile="33" probability="1"/>
+   <wangtile tileid="4" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="5" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="6" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="7" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="9" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="29" wangid="0,1,0,1,0,2,0,2"/>
    <wangtile tileid="30" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="31" wangid="0,0,0,0,0,1,0,1"/>
-   <wangtile tileid="54" wangid="0,1,0,0,0,0,0,0"/>
-   <wangtile tileid="55" wangid="0,1,0,0,0,0,0,1"/>
-   <wangtile tileid="56" wangid="0,0,0,0,0,0,0,1"/>
-   <wangtile tileid="57" wangid="0,0,0,1,0,1,0,1"/>
-   <wangtile tileid="59" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="31" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="33" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="54" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="55" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="56" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="57" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="59" wangid="0,1,0,1,0,1,0,2"/>
   </wangset>
  </wangsets>
 </tileset>
